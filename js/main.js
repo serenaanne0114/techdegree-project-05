d3c2704b1b3b72f55818b9f834bf3b8bbb17f058
serenaanne0114 <56902316+serenaanne0114@users.noreply.github.com>
2020-05-14 19:32:08 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-//Lightbox
+
 
 //searchbox
 $(document).ready(function() {
-    let 
-})
+    let images = getElementsByTagName('a');
+    $('#search').on('keyup', function(){
+        let search = $('#search').val().toLowerCase;
+        for(let i=0; i<images.length; i++) {
+            let searchVal = images[i].getAttribute('data-title');
+            if (searchVal.toLowerCase().indexOf(search) > -1) {
+                images[i].style.display = "";
+            } else {
+                images[i].style.display = none;
+            }
+        }
+    });
+});
 
 
 
