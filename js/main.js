92d5280256f37dc9d3b7b303449be3927053f07b
serenaanne0114 <56902316+serenaanne0114@users.noreply.github.com>
2020-05-27 20:37:52 -0700

    update

--- js/main.js
+++ js/main.js
@@ -9,16 +9,17 @@ lightbox.option({
 })
 
 //searchbox
-$(document).ready(function() {
-    let images = getElementsByTagName('a');
-    $('#search').on('keyup', function(){
+const newLocal = $(document).ready(function () {
+    let images = document.getElementsByTagName('a');
+    $('#search').on('keyup', function () {
         let search = $('#search').val().toLowerCase;
-        for(let i=0; i<images.length; i++) {
+        for (let i = 0; i < images.length; i++) {
             let searchVal = images[i].getAttribute('data-title');
             if (searchVal.toLowerCase().indexOf(search) > -1) {
                 images[i].style.display = "";
-            } else {
-                images[i].style.display = none;
+            }
+            else {
+                images[i].style.display = 'none';
             }
         }
     });
