4f7db59b1de9b30cabbf3629b8e754d4f56237d0
serenaanne0114 <56902316+serenaanne0114@users.noreply.github.com>
2020-06-03 21:14:26 -0700

    update

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ lightbox.option({
 })
 
 //searchbox
-const newLocal = $(document).ready(function () {
-    let images = document.getElementsByTagName('a');
+const searchBox= $(document).ready(function () {
+    const images = document.getElementsByTagName('a');
     $('#search').on('keyup', function () {
-        let search = $('#search').val().toLowerCase;
+        const search = $('#search').val().toLowerCase;
         for (let i = 0; i < images.length; i++) {
-            let searchVal = images[i].getAttribute('data-title');
+            const searchVal = images[i].getAttribute('data-title');
             if (searchVal.toLowerCase().indexOf(search) > -1) {
-                images[i].style.display = "";
+                images[i].style.display = '';
             }
             else {
                 images[i].style.display = 'none';
